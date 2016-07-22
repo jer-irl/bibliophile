13a61009899597e173cc41b675aedd1a3c016353
James Waugh <jtwaugh@users.noreply.github.com>
2016-07-21 19:56:40 -0500

    Added whitespace

--- src/Tile.js
+++ src/Tile.js
@@ -30,31 +30,31 @@ function Tile(theI, theJ, theLett) {
 	// I'm doing them as functions to avoid recursive explosions, because I don't trust javascript with any sort of lazy initializations.
 
 	if (this.i % 2 == 1) {
-		this.upRight = function () { return new Position(i+1, j); }
-		this.upLeft = function () { return new Position(i-1, j); }
-		this.downRight = function () { return new Position(i+1, j-1); }
-		this.downLeft = function () { new Position(i-1, j-1); }
+		this.upRight = function () { return new Position(i + 1, j); }
+		this.upLeft = function () { return new Position(i - 1, j); }
+		this.downRight = function () { return new Position(i + 1, j - 1); }
+		this.downLeft = function () { new Position(i - 1, j - 1); }
 	} else if (this.i % 2 == 0) {
-		this.upRight = function () { return new Position(i+1, j+1); }
-		this.upLeft = function () { return new Position(i-1, j+1); }
-		this.downRight = function () { return new Position(i+1, j); }
-		this.downLeft = function () { return new Position(i-1, j); }
+		this.upRight = function () { return new Position(i + 1, j + 1); }
+		this.upLeft = function () { return new Position(i - 1, j + 1); }
+		this.downRight = function () { return new Position(i + 1, j); }
+		this.downLeft = function () { return new Position(i - 1, j); }
 	}
 
 	this.above = function() {
-		return new Position(i, j-1);
+		return new Position(i, j - 1);
 	}
 
 	this.below = function() {
-		return new Position(i, j+1);
+		return new Position(i, j + 1);
 	}
 
 	this.right = function() {
-		return new Position(i+1, j);
+		return new Position(i + 1, j);
 	}
 
 	this.left = function() {
-		return new Position(i-1, j);
+		return new Position(i - 1, j);
 	}
 
 	this.updateDisplayCoords = function() {
