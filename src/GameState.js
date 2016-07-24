0e9f0ae4198c435669bc239890d7cae9d536122c
Jeremy Schroeder <jpschroeder2014@gmail.com>
2016-07-24 19:41:24 -0400

    Made methods for prototypes, redid tile neighbor

--- src/GameState.js
+++ src/GameState.js
@@ -13,128 +13,128 @@ function GameState() {
 	this.submittedWords = [];
 	this.wordQualityIndex = 0;
 
+}
 
-	// Readonly Methods:
-	// ---------------------------------------------------------------------------
 
-	/**
-	 * Returns the selected word from the chain of the gamestate
-	 * @returns {String} The selected string
-	 */
-	this.selectedWord = function() {
-		var candidate = "";
-		for (var iter = 0; iter < this.selectionChain.length; iter++){
-			candidate += this.gameboard[this.selectionChain[iter].j][this.selectionChain[iter].i].lett;
-		}
-		return candidate;
+// Readonly Methods:
+// ---------------------------------------------------------------------------
+
+/**
+ * Returns the selected word from the chain of the gamestate
+ * @returns {String} The selected string
+ */
+GameState.prototype.selectedWord = function() {
+	var candidate = "";
+	for (var iter = 0; iter < this.selectionChain.length; iter++){
+		candidate += this.gameboard[this.selectionChain[iter].j][this.selectionChain[iter].i].lett;
 	}
+	return candidate;
+}
 
-	/**
-	 * Determines whether a selection collides with the already selected chain.
-	 * @param {Position} The candidate position
-	 * @return {Boolean} true if collision
-	 */
-	this.selectionCollides = function(selpos) {
-		var collides = false;
+/**
+ * Determines whether a selection collides with the already selected chain.
+ * @param {Position} The candidate position
+ * @return {Boolean} true if collision
+ */
+GameState.prototype.selectionCollides = function(selpos) {
+	var collides = false;
 
-		// For each tile, check
-		for (var iter = 0; iter < this.selectionChain.length - 1; iter++) {
-			// If we did hit it, delete from the chain all tiles ahead of the selected one
-			if (selpos.i == this.selectionChain[iter].i && selpos.j == this.selectionChain[iter].j) {
-				collides = true;
-				break;
-			}
-		}
-		return collides;
-	}
-
-	// Mutating Methods:
-	// ---------------------------------------------------------------------------
-
-	/**
-	 * Pushes a selection to the selection chain
-	 * @param {Position} input coordinates (selpos)
-	 */
-	this.pushToSelectionChain = function(tile) {
-		this.selectionChain.push(tile);
-		return;
-	}
-
-	/**
-	 * Pops the last n items from the selection chain
-	 * @param {number} number of indexes to pop
-	 */
-	this.popFromSelectionChain = function(n) {
-		var k = this.selectionChain.length - n - 1;
-		for (var j = 0; j < k; j++){
-			this.selectionChain.pop();
+	// For each tile, check
+	for (var iter = 0; iter < this.selectionChain.length - 1; iter++) {
+		// If we did hit it, delete from the chain all tiles ahead of the selected one
+		if (selpos.i == this.selectionChain[iter].i && selpos.j == this.selectionChain[iter].j) {
+			collides = true;
+			break;
 		}
 	}
+	return collides;
+}
 
-	/**
-	 * Clears the selection chain
-	 */
-	this.clearSelectionChain = function() {
-		this.selectionChain = [];
+// Mutating Methods:
+// ---------------------------------------------------------------------------
+
+/**
+ * Pushes a selection to the selection chain
+ * @param {Position} input coordinates (selpos)
+ */
+GameState.prototype.pushToSelectionChain = function(tile) {
+	this.selectionChain.push(tile);
+	return;
+}
+
+/**
+ * Pops the last n items from the selection chain
+ * @param {number} number of indexes to pop
+ */
+GameState.prototype.popFromSelectionChain = function(n) {
+	var k = this.selectionChain.length - n - 1;
+	for (var j = 0; j < k; j++){
+		this.selectionChain.pop();
 	}
+}
 
-	// TODO
-	this.shuffleBoard = function() {
-		throw "Not implemented";
-		return;
-	};
+/**
+ * Clears the selection chain
+ */
+GameState.prototype.clearSelectionChain = function() {
+	this.selectionChain = [];
+}
 
-	/**
-	 * Adds the input word to the list of submitted words, and to the score
-	 * @param {String} word to accept
-	 */
-	this.addSubmittedWord = function(word) {
-		this.submittedWords.push(word);
-		this.score += baseWordScore(word);
-		return;
-	}
+// TODO
+GameState.prototype.shuffleBoard = function() {
+	throw "Not implemented";
+	return;
+};
 
-	/**
-	 * Get the default game board, with each tile selected by weightedChar
-	 */
-	this.initGameboard = function() {
-		for (var j = 0; j < 8; j++) {
-			this.gameboard.push([]);
-			for (var i = 0; i < 7; i++) {
-				var tileToPush = new Tile(i, j, weightedChar());
-				this.gameboard[j].push(tileToPush);
-			}
+/**
+ * Adds the input word to the list of submitted words, and to the score
+ * @param {String} word to accept
+ */
+GameState.prototype.addSubmittedWord = function(word) {
+	this.submittedWords.push(word);
+	this.score += baseWordScore(word);
+	return;
+}
+
+/**
+ * Get the default game board, with each tile selected by weightedChar
+ */
+GameState.prototype.initGameboard = function() {
+	for (var j = 0; j < 8; j++) {
+		this.gameboard.push([]);
+		for (var i = 0; i < 7; i++) {
+			var tileToPush = new Tile(i, j, weightedChar());
+			this.gameboard[j].push(tileToPush);
 		}
-		return;
 	}
+	return;
+}
 
-	this.toString = function() {
-		var result = "";
+GameState.prototype.toString = function() {
+	var result = "";
 
-		// Gameboard
-		result += "Gameboard:\n";
+	// Gameboard
+	result += "Gameboard:\n";
+	result += "-----------------------\n";
+	//result += "|   |   |   |   |   |   |   |\n";
+	for (var row = 0; row < 8; row++) {
+		result += "| ";
+		for (var col = 0; col < 7; col++) {
+			result += this.gameboard[row][col].lett + " | ";
+		}
+		result += "\n";
+
+		//result += "|   |   |   |   |   |   |   |\n";
 		result += "-----------------------\n";
 		//result += "|   |   |   |   |   |   |   |\n";
-		for (var row = 0; row < 8; row++) {
-			result += "| ";
-			for (var col = 0; col < 7; col++) {
-				result += this.gameboard[row][col].lett + " | ";
-			}
-			result += "\n";
-
-			//result += "|   |   |   |   |   |   |   |\n";
-			result += "-----------------------\n";
-			//result += "|   |   |   |   |   |   |   |\n";
-		}
-
-		// Selection chain
-		result += "\nSelection Chain:\n"
-		for (var iter = 0; iter < this.selectionChain.length; iter++) {
-			result += this.selectionChain[iter].toString();
-		}
-
-		return result;
 	}
 
+	// Selection chain
+	result += "\nSelection Chain:\n"
+	for (var iter = 0; iter < this.selectionChain.length; iter++) {
+		result += this.selectionChain[iter].toString();
+	}
+
+	return result;
 }
 
--- src/Tile.js
+++ src/Tile.js
@@ -8,93 +8,117 @@ function Tile(theI, theJ, theLett) {
 	// Letter
 	this.lett = theLett;
 
-	this.coordToPlace = function() {
-		var tileWidth = Globals.tileWidth;
-		var tileHeight = Globals.tileHeight;
-		var tileSpace = Globals.tileSpace;
-		var boardX = Globals.boardX;
-		var boardY = Globals.boardY;
-
-		var theX = this.i * (tileWidth + tileSpace) + boardX;
-		var theY = this.j * (tileHeight + tileSpace) + ((tileHeight / 2) * (this.i % 2)) + boardY;
-		return {x: theX, y: theY};
-	}
-
 	// Coordinate for Drawing function
 	this.x = this.coordToPlace().x;
 	this.y = this.coordToPlace().y;
 
-	// Direction it needs to be moved.  'none', 'down' valid
-	this.directionOfTravel = 'none';
+}
 
-	// I'm doing them as functions to avoid recursive explosions, because I don't trust javascript with any sort of lazy initializations.
 
+// Neighbor accessors
+
+/**
+ * Dir is a string: up, down, left, right, upLeft, upRight, downLeft, downRight
+ */
+Tile.prototype.neighbor = function(direc) {
+	// Cardinal directions
+	if (direc == "up") {
+		return gameState.gameboard[this.j - 1][this.i];
+	} else if (direc == "down") {
+		return gameState.gameboard[this.j + 1][this.i];
+	} else if (direc == "right") {
+		return gameState.gameboard[this.j][this.i + 1];
+	} else if (direc == "left") {
+		return gameState.gameboard[this.j][this.i - 1];
+	}
+
+	// Diagonals:
 	if (this.i % 2 == 1) {
-		this.upRight = function () { return new Position(i + 1, j); }
-		this.upLeft = function () { return new Position(i - 1, j); }
-		this.downRight = function () { return new Position(i + 1, j - 1); }
-		this.downLeft = function () { new Position(i - 1, j - 1); }
+		if (direc == "upRight") {
+			return gameState.gameboard[this.j][this.i + 1];
+		} else if (direc == "upLeft") {
+			return gameState.gameboard[this.j][this.i - 1];
+		} else if (direc == "downRight") {
+			return gameState.gameboard[this.j - 1][this.i + 1];
+		} else if (direc == "downLeft") {
+			return gameState.gameboard[this.j - 1][this.i - 1];
+		}
 	} else if (this.i % 2 == 0) {
-		this.upRight = function () { return new Position(i + 1, j + 1); }
-		this.upLeft = function () { return new Position(i - 1, j + 1); }
-		this.downRight = function () { return new Position(i + 1, j); }
-		this.downLeft = function () { return new Position(i - 1, j); }
-	}
-
-	this.above = function() {
-		return new Position(i, j - 1);
-	}
-
-	this.below = function() {
-		return new Position(i, j + 1);
-	}
-
-	this.right = function() {
-		return new Position(i + 1, j);
-	}
-
-	this.left = function() {
-		return new Position(i - 1, j);
-	}
-
-	this.updateDisplayCoords = function() {
-		// If already happily in place, do nothing
-		if (this.x == this.coordToPlace().x &&
-		    this.y == this.coordToPlace().y) {
-			return;
-		}
-		// Else, move y one down
-		else {
-			this.y += 1;
-			return;
+		if (direc == "upRight") {
+			return gameState.gameboard[this.j + 1][this.i + 1];
+		} else if (direc == "upLeft") {
+			return gameState.gameboard[this.j + 1][this.i - 1];
+		} else if (direc == "downRight") {
+			return gameState.gameboard[this.j][this.i + 1];
+		} else if (direc == "downLeft") {
+			return gameState.gameboard[this.j][this.i - 1];
 		}
 	}
 
-	/**
-	 * Draw a single tile
-	 * @param {Position} grid index
-	 * @param {Color} fill color for tile
-	 */
-	this.drawTile = function(color) {
-		console.log("drawTile called for" + this);
-		Globals.ctx.fillStyle=color;
-		Globals.ctx.fillRect(this.x, this.y, Globals.tileWidth, Globals.tileHeight);
-		Globals.ctx.font="35px Garamond";
-		Globals.ctx.fillStyle="#000000";
-		Globals.ctx.rect(this.x, this.y, Globals.tileWidth, Globals.tileHeight);
-		var text = Globals.ctx.measureText(gameState.gameboard[this.j][this.i].lett);
-		var xOffset = (Globals.tileWidth - text.width) / 2;
-		var yOffset = (Globals.tileHeight - text.height) / 2;
+	throw "Invalid input to neighbor";
+	return;
 
-		Globals.ctx.fillText(gameState.gameboard[this.j][this.i].lett, this.x + xOffset, this.y + 35);
-	}
+}
 
-	this.toString = function() {
-		var out = "";
-		out += "Tile(i: " + this.i + ", ";
-		out += "j: " + this.j + ", ";
-		out += "lett: " + this.lett + ")\n";
-		return out;
+
+// Drawing Methods
+Tile.prototype.updateDisplayCoords = function() {
+	// If already happily in place, do nothing
+	if (this.x == this.coordToPlace().x &&
+		this.y == this.coordToPlace().y) {
+		return;
 	}
+	// Else, move y one down
+	else {
+		this.y += 1;
+		return;
+	}
+}
+
+Tile.prototype.coordToPlace = function() {
+	var tileWidth = Globals.tileWidth;
+	var tileHeight = Globals.tileHeight;
+	var tileSpace = Globals.tileSpace;
+	var boardX = Globals.boardX;
+	var boardY = Globals.boardY;
+
+	var theX = this.i * (tileWidth + tileSpace) + boardX;
+	var theY = this.j * (tileHeight + tileSpace) + ((tileHeight / 2) * (this.i % 2)) + boardY;
+	return {x: theX, y: theY};
+}
+
+/**
+ * Draw a single tile
+ * @param {Position} grid index
+ * @param {Color} fill color for tile
+ */
+Tile.prototype.drawTile = function(color) {
+	console.log("drawTile called for" + this);
+	Globals.ctx.fillStyle=color;
+	Globals.ctx.fillRect(this.x, this.y, Globals.tileWidth, Globals.tileHeight);
+	Globals.ctx.font="35px Garamond";
+	Globals.ctx.fillStyle="#000000";
+	Globals.ctx.rect(this.x, this.y, Globals.tileWidth, Globals.tileHeight);
+	var text = Globals.ctx.measureText(gameState.gameboard[this.j][this.i].lett);
+	var xOffset = (Globals.tileWidth - text.width) / 2;
+	var yOffset = (Globals.tileHeight - text.height) / 2;
+
+	Globals.ctx.fillText(gameState.gameboard[this.j][this.i].lett, this.x + xOffset, this.y + 35);
+}
+
+Tile.prototype.showDeletion = function() {
+	console.log("Deletion Animation not implemented");
+	return;
+}
+
+
+// Debug Methods
+
+Tile.prototype.toString = function() {
+	var out = "";
+	out += "Tile(i: " + this.i + ", ";
+	out += "j: " + this.j + ", ";
+	out += "lett: " + this.lett + ")\n";
+	return out;
 }
 
